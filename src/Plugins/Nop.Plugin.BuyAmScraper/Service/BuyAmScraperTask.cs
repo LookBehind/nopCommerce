fcd17646699071d6bf0100a89e302fd05bc06978
PWND <PWND@PWND.COM>
2021-07-04 00:11:34 +0400

    changed way of scraping

--- src/Plugins/Nop.Plugin.BuyAmScraper/Service/BuyAmScraperTask.cs
+++ src/Plugins/Nop.Plugin.BuyAmScraper/Service/BuyAmScraperTask.cs
@@ -10,22 +10,37 @@ using PuppeteerSharp;
 using Nop.Web.Areas.Admin.Infrastructure.Mapper.Extensions;
 using Nop.Services.Customers;
 using Nop.Services.Vendors;
-using System.Collections.Generic;
 using Nop.Services.Media;
 using Nop.Core;
 using Nop.Services.Seo;
+using HtmlAgilityPack;
+using System.Text.Json;
+using System.Diagnostics;
 
 namespace Nop.Plugin.BuyAmScraper.Service
 {
-    class BuyAmScraperTask : Services.Tasks.IScheduleTask
+    class AjaxResponse
     {
+        public int TotalCount { get; set; }
+        public string Listing { get; set; }
+        public string Pagination { get; set; }
+    }
+
+    class ProductMiniDto
+    {
+        public int Sku { get; set; }
+        public string FullProductUrl { get; set; }
+    }
+
+    public class BuyAmScraperTask : Services.Tasks.IScheduleTask
+    {
+        private const bool EXTRACT_FROM_RENDERED_PAGES = false;
         private const int SCROLL_HEIGHT = 300;
         private const string CARREFOUR_CUSTOMER_NAME = "Carrefour";
         private readonly string[] _categoryUrlsToScrape;
 
         private ILogger _logger;
         private IProductService _productService;
-        private ICustomerService _customerService;
         private IVendorService _vendorService;
         private ICategoryService _categoryService;
         private IPictureService _pictureService;
@@ -33,50 +48,31 @@ namespace Nop.Plugin.BuyAmScraper.Service
 
         public BuyAmScraperTask(ILogger logger, 
             IProductService productService, 
-            ICustomerService customerService, 
             IVendorService vendorService,
             ICategoryService categoryService,
             IPictureService pictureService,
             IUrlRecordService urlRecordService)
         {
             this._categoryUrlsToScrape = new string[] {
-                //"https://buy.am/hy/carrefour/bakery-pastry",
-                //"https://buy.am/hy/carrefour/fresh-fruit-vegetable",
-                //"https://buy.am/hy/carrefour/dairy-eggs",
-                //"https://buy.am/hy/carrefour/frozen-products",
-                //"https://buy.am/hy/carrefour/breakfast-coffee-tea",
-                //"https://buy.am/hy/carrefour/bio-organic",
-                //"https://buy.am/hy/carrefour/sweets-snacks",
-                //"https://buy.am/hy/carrefour/juices-drinks",
-                //"https://buy.am/hy/carrefour/alcoholic-beverages-cigarettes",
+                "https://buy.am/hy/carrefour/bakery-pastry",
+                "https://buy.am/hy/carrefour/fresh-fruit-vegetable",
+                "https://buy.am/hy/carrefour/dairy-eggs",
+                "https://buy.am/hy/carrefour/frozen-products",
+                "https://buy.am/hy/carrefour/breakfast-coffee-tea",
+                "https://buy.am/hy/carrefour/bio-organic",
+                "https://buy.am/hy/carrefour/sweets-snacks",
+                "https://buy.am/hy/carrefour/juices-drinks",
+                "https://buy.am/hy/carrefour/alcoholic-beverages-cigarettes",
                 
                 "https://buy.am/hy/carrefour/shop-in-shop",
                 "https://buy.am/hy/carrefour/household-goods",
             };
             this._logger = logger;
             this._productService = productService;
-            this._customerService = customerService;
             this._vendorService = vendorService;
             this._categoryService = categoryService;
             this._pictureService = pictureService;
             this._urlRecordService = urlRecordService;
-        }
-
-        private async Task ScrollUntilEnd(Page page)
-        {
-            int scrolledHeight = 0;
-            while (true)
-            {
-                await page.Mouse.WheelAsync(0, SCROLL_HEIGHT);
-                scrolledHeight += SCROLL_HEIGHT;
-                await Task.Delay(300);
-                var bodyScrollHeight = await page.EvaluateExpressionAsync<int>("document.body.scrollHeight");
-                if (scrolledHeight >= bodyScrollHeight)
-                {
-                    await _logger.InformationAsync("Scrolled to the page end");
-                    break;
-                }
-            }
         }
 
         private async Task<byte[]> DownloadImage(string url)
@@ -94,8 +90,127 @@ namespace Nop.Plugin.BuyAmScraper.Service
             return Array.Empty<byte>();
         }
 
-        private async Task<IReadOnlyList<ProductDTO>> ExtractProducts(Page page)
+        private async Task<ProductDTO> Convert(ProductMiniDto miniDto)
         {
+            using var httpClient = new WebClient();
+
+            var productPageData = await httpClient.DownloadStringTaskAsync(miniDto.FullProductUrl);
+
+            var productPageHtml = new HtmlDocument();
+            productPageHtml.LoadHtml(productPageData);
+
+            var productTitle = productPageHtml.DocumentNode.SelectSingleNode("//h1[@class='product--title']");
+            var category = productPageHtml.DocumentNode.SelectSingleNode("//li[@class = 'breadcrumb--entry is--active']//span[@class = 'breadcrumb--title']");
+            var subCategory = productPageHtml.DocumentNode.SelectSingleNode("//span[normalize-space()='Բաժին']/following-sibling::a");
+            var fullDescription = productPageHtml.DocumentNode.SelectSingleNode("//div[contains(@class, 'product--description')]");
+            var price = productPageHtml.DocumentNode.SelectSingleNode("//meta[@itemprop='price']");
+            var imageUrl = productPageHtml.DocumentNode.SelectSingleNode("//span[@class='image--element']");
+
+            var imageUrlParsed = new Uri(imageUrl.Attributes["data-img-original"].Value);
+            var imageBytes = await DownloadImage(imageUrlParsed.ToString());
+
+            var fullDescriptionString = fullDescription?.InnerText ?? string.Empty;
+
+            return new ProductDTO {
+                Name = productTitle.InnerText,
+                Code = miniDto.Sku,
+                Category = category.InnerText,
+                FullDescription = $"Code: {miniDto.Sku}\n" + fullDescriptionString,
+                Price = int.Parse(price.Attributes["content"].Value, System.Globalization.NumberStyles.AllowDecimalPoint),
+                ShortDescription = $"Code: {miniDto.Sku}\n" + fullDescriptionString.Substring(0, Math.Min(100, fullDescriptionString.Length)),
+                SubCategory = subCategory?.InnerText ?? string.Empty,
+                Image = imageBytes,
+                ImageFileName = imageUrlParsed.Segments[imageUrlParsed.Segments.Length - 1]
+            };
+        }
+
+        private async IAsyncEnumerable<ProductMiniDto> ExtractProductsFromDownloadedPages(string categoryUrl)
+        {
+            using var httpClient = new WebClient();
+            string categoryPageData = null;
+            try
+            {
+                categoryPageData = await httpClient.DownloadStringTaskAsync(categoryUrl);
+            }
+            catch(Exception exc)
+            {
+                await _logger.ErrorAsync($"Exception while downloading page: {categoryUrl}, message: {exc.Message}");
+                yield break;
+            }
+
+            var categoryPageHtml = new HtmlDocument();
+            categoryPageHtml.LoadHtml(categoryPageData);
+            var ajaxRequestUrl = categoryPageHtml.DocumentNode.SelectSingleNode("//form[@id='filter']");
+            var ub = new UriBuilder(ajaxRequestUrl.Attributes["data-listing-url"].Value);
+
+            for (int i = 1; i < 500; i++)
+            {
+                ub.Query = $"p={i}&c={ub.Uri.Segments[ub.Uri.Segments.Length - 1]}&imRootCategoryId=3&o=1&n=100&loadProducts=1";
+
+                AjaxResponse productsResponse = null;
+                try
+                {
+                    productsResponse = JsonSerializer.Deserialize<AjaxResponse>(
+                        await httpClient.DownloadStringTaskAsync(ub.Uri),
+                        new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+                    );
+                }
+                catch (Exception exc)
+                {
+                    await _logger.ErrorAsync($"Exception while getting results from: {ub.Uri}, message: {exc.Message}");
+                    yield break;
+                }
+
+                var productsPageHtml = new HtmlDocument();
+                productsPageHtml.LoadHtml(productsResponse.Listing);
+
+                var productSkusCurrentNodes = productsPageHtml.DocumentNode
+                    .SelectNodes("/div[@class='product--box box--minimal']");
+
+                if (productSkusCurrentNodes == null)
+                    break;
+
+                var productSkusCurrent = productSkusCurrentNodes
+                    .Select(x => {
+                        return new ProductMiniDto
+                        {
+                            Sku = int.Parse(x.Attributes["data-ordernumber"].Value),
+                            FullProductUrl = x.SelectSingleNode(".//a[@class='product--title']").Attributes["href"].Value
+                        };
+                    });
+                if (productSkusCurrent == null || !productSkusCurrent.Any())
+                    break;
+
+                foreach (var productSku in productSkusCurrent)
+                {
+                    yield return productSku;
+                }
+            }
+        }
+
+        private async Task<IReadOnlyList<ProductDTO>> ExtractProductsFromRenderedPage(Browser browser, string categoryUrl)
+        {
+            async Task ScrollUntilEnd(Page page)
+            {
+                int scrolledHeight = 0;
+                while (true)
+                {
+                    await page.Mouse.WheelAsync(0, SCROLL_HEIGHT);
+                    scrolledHeight += SCROLL_HEIGHT;
+                    await Task.Delay(300);
+                    var bodyScrollHeight = await page.EvaluateExpressionAsync<int>("document.body.scrollHeight");
+                    if (scrolledHeight >= bodyScrollHeight)
+                    {
+                        await _logger.InformationAsync("Scrolled to the page end");
+                        break;
+                    }
+                }
+            }
+
+            await using var page = await browser.NewPageAsync();
+            await page.GoToAsync(categoryUrl);
+            await ScrollUntilEnd(page);
+
             var products = new List<ProductDTO>();
             var productsInDom = await page.QuerySelectorAllAsync("div.product--box.box--minimal .image--media img");
             var categoryName = await page.EvaluateExpressionAsync<string>("document.querySelector('li.is--active span.breadcrumb--title').innerText");
@@ -193,7 +308,7 @@ namespace Nop.Plugin.BuyAmScraper.Service
             return categories.FirstOrDefault(c => string.Equals(c.Name, name));
         }
 
-        async Task AddProductsIfMissing(IReadOnlyList<ProductDTO> productDTOs)
+        async Task AddProductsIfMissing(IAsyncEnumerable<object> productDTOs)
         {
             var carrefourVendor = (await _vendorService.GetAllVendorsAsync(CARREFOUR_CUSTOMER_NAME)).FirstOrDefault();
             if(carrefourVendor == null)
@@ -202,14 +317,24 @@ namespace Nop.Plugin.BuyAmScraper.Service
                 return;
             }
 
-            foreach (var productDTO in productDTOs)
+            await foreach (var productDTOObject in productDTOs)
             {
-                var existingProduct = await _productService.GetProductBySkuAsync(productDTO.Code.ToString());
+                var productMiniDTO = productDTOObject as ProductMiniDto;
+                var productDTO = productDTOObject as ProductDTO;
+                if (productDTO == null && productMiniDTO == null)
+                    break;
+
+                var productCode = productDTO?.Code ?? productMiniDTO.Sku;
+
+                var existingProduct = await _productService.GetProductBySkuAsync(productCode.ToString());
                 if (existingProduct != null)
                 {
-                    await _logger.InformationAsync($"Product with code {productDTO.Code} exists, skipping");
+                    await _logger.InformationAsync($"Product with code {productCode} exists, skipping");
                     continue;
                 }
+
+                if (productMiniDTO != null)
+                    productDTO = await Convert(productMiniDTO);
 
                 var picture = await _pictureService.InsertPictureAsync(productDTO.Image, MimeTypes.ImageJpeg, productDTO.ImageFileName);
 
@@ -230,6 +355,9 @@ namespace Nop.Plugin.BuyAmScraper.Service
                 };
 
                 await _productService.InsertProductAsync(product);
+
+                var seName = await _urlRecordService.ValidateSeNameAsync(product, null, product.Name, true);
+                await _urlRecordService.SaveSlugAsync(product, seName, 0);
 
                 var vendorCategory = await GetExactCategoryByName(CARREFOUR_CUSTOMER_NAME);
                 if(vendorCategory == null)
@@ -281,9 +409,9 @@ namespace Nop.Plugin.BuyAmScraper.Service
                     });
                 }
 
-                bool haveSubCategorySpecified = productDTO.SubCategory == null ? true : false;
-                var productSubCategory = haveSubCategorySpecified ? await GetExactCategoryByName(productDTO.SubCategory) : null;
-                if (haveSubCategorySpecified && productSubCategory == null)
+                bool subCategorySpecified = string.IsNullOrEmpty(productDTO.SubCategory) ? false : true;
+                var productSubCategory = subCategorySpecified ? await GetExactCategoryByName(productDTO.SubCategory) : null;
+                if (subCategorySpecified && productSubCategory == null)
                 {
                     productSubCategory = new Core.Domain.Catalog.Category
                     {
@@ -309,7 +437,7 @@ namespace Nop.Plugin.BuyAmScraper.Service
 
                 await _categoryService.InsertProductCategoryAsync(new Core.Domain.Catalog.ProductCategory {
                     ProductId = product.Id,
-                    CategoryId = haveSubCategorySpecified ? productSubCategory.Id : productCategory.Id
+                    CategoryId = subCategorySpecified ? productSubCategory.Id : productCategory.Id
                 });
 
                 await _productService.InsertProductPictureAsync(new Core.Domain.Catalog.ProductPicture
@@ -317,6 +445,8 @@ namespace Nop.Plugin.BuyAmScraper.Service
                     PictureId = picture.Id,
                     ProductId = product.Id
                 });
+
+                await _logger.InformationAsync($"Product Name={productDTO.Name} have been imported");
             }
             
         }
@@ -325,23 +455,42 @@ namespace Nop.Plugin.BuyAmScraper.Service
         {
             int result = 0;
 
-            var browserFetcher = new BrowserFetcher();
-            await browserFetcher.DownloadAsync();
-            await using var browser = await Puppeteer.LaunchAsync(
-                new LaunchOptions { Headless = false, DefaultViewport = new ViewPortOptions { Width = 1920, Height = 1024 } });
+            Browser browser = null;
 
-            foreach (var categoryUrl in _categoryUrlsToScrape)
+            try
             {
-                await using var page = await browser.NewPageAsync();
-                await page.GoToAsync(categoryUrl);
-                await ScrollUntilEnd(page);
+                if (EXTRACT_FROM_RENDERED_PAGES)
+                {
+                    var browserFetcher = new BrowserFetcher();
+                    await browserFetcher.DownloadAsync();
+                    browser = await Puppeteer.LaunchAsync(
+                        new LaunchOptions { Headless = false, DefaultViewport = new ViewPortOptions { Width = 1920, Height = 1024 } });
+                }
 
-                var products = await ExtractProducts(page);
-                await AddProductsIfMissing(products);
+                foreach (var categoryUrl in _categoryUrlsToScrape)
+                {
+                    var sw = new Stopwatch();
+                    sw.Start();
 
-                result += products.Count;
+                    var products =   
+                        //ExtractProductsFromRenderedPage(browser, categoryUrl)
+                        ExtractProductsFromDownloadedPages(categoryUrl)
+                        ;
+                    await AddProductsIfMissing(products);
+
+                    var count = await products.CountAsync();
+                    result += count;
+                    
+                    sw.Stop();
+                    await _logger.InformationAsync($"Finished category {categoryUrl}, {count} products, took {sw.Elapsed.Seconds} seconds");
+                }
             }
-
+            finally
+            {
+                if (browser != null)
+                    await browser.DisposeAsync();
+            }
+            
             return result;
         }
 
