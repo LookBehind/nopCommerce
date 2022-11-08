baa0142cdfdaa8fd0aba88850254866089a7ac36
artashes-khalatyan <artashes.khalatyan1986@gmail.com>
2022-11-08 22:59:27 +0400

    Add migration to add cancellation message template

--- src/Libraries/Nop.Data/Migrations/UpgradeTo450/DataMigration.cs
+++ src/Libraries/Nop.Data/Migrations/UpgradeTo450/DataMigration.cs
@@ -1,4 +1,7 @@
-﻿using FluentMigrator;
+﻿using System;
+using System.Linq;
+using FluentMigrator;
+using Nop.Core.Domain.Messages;
 
 namespace Nop.Data.Migrations.UpgradeTo450
 {
@@ -18,6 +21,20 @@ namespace Nop.Data.Migrations.UpgradeTo450
         /// </summary>
         public override void Up()
         {
+            var record = _dataProvider.GetTable<MessageTemplate>().Where(e => e.Name == "OrderCancelled.VendorNotification").FirstOrDefault();
+            if (record == null)
+            {
+                _dataProvider.InsertEntity(new MessageTemplate()
+                {
+                    Name = "OrderCancelled.VendorNotification",
+                    Subject = "%Store.Name%. Order Cancelled",
+                    Body = "%Customer.FullName% has just cancelled the order." + Environment.NewLine +
+                            "Order #: %Order.OrderNumber%" + Environment.NewLine +
+                            "Shipping Address: %Order.ShippingAddress1%, %Order.ShippingAddress2%" + Environment.NewLine +
+                            "%Order.ProductsHumanReadable%",
+                    IsActive = true,
+                });
+            }
         }
 
         public override void Down()
