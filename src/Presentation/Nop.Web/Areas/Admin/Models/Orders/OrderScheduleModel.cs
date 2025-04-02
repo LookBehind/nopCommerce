b37258e6e59ef4650dbb828081889cb01df15186
LookBehind <ed.isajanyan@gmail.com>
2025-04-03 02:24:35 +0400

    dynamic schedule dates

--- src/Presentation/Nop.Web/Areas/Admin/Models/Orders/OrderScheduleModel.cs
+++ src/Presentation/Nop.Web/Areas/Admin/Models/Orders/OrderScheduleModel.cs
@@ -1,20 +1,12 @@
-﻿using Nop.Web.Framework.Models;
+﻿using System.Collections.Generic;
+using Nop.Web.Framework.Models;
 using Nop.Web.Framework.Mvc.ModelBinding;
 
 namespace Nop.Web.Areas.Admin.Models.Orders
 {
     public partial class OrderScheduleModel
     {
-        [NopResourceDisplayName("Admin.Orders.Fields.ScheduleDate1")]
-        public string ScheduleDate1 { get; set; }
-
-        [NopResourceDisplayName("Admin.Orders.Fields.ScheduleDate2")]
-        public string ScheduleDate2 { get; set; }
-
-        //[NopResourceDisplayName("Admin.Orders.Fields.ScheduleDate3")]
-        //public string ScheduleDate3 { get; set; }
-
-        //[NopResourceDisplayName("Admin.Orders.Fields.ScheduleDate4")]
-        //public string ScheduleDate4 { get; set; }
+        [NopResourceDisplayName("Admin.Orders.Fields.ScheduleDate")]
+        public List<string> ScheduleDates { get; set; }
     }
 }
