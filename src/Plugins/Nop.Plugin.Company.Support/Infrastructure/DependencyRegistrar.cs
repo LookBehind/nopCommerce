05a9150e8bea51a3f6618170cf1b5e4f1d2b82d3
Edgar Isajanyan <ed.isajanyan@gmail.com>
2026-09-25 17:34:35 +0400

    Fix Company.Support DI: register ISupportCaseModelFactory
    SupportCaseController resolution was throwing Autofac.Core.DependencyResolutionException
    on every admin request (found via the Log table after installing the plugin) - the
    factory implementation was never added to the plugin's DependencyRegistrar.

--- src/Plugins/Nop.Plugin.Company.Support/Infrastructure/DependencyRegistrar.cs
+++ src/Plugins/Nop.Plugin.Company.Support/Infrastructure/DependencyRegistrar.cs
@@ -2,6 +2,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Nop.Core.Configuration;
 using Nop.Core.Infrastructure;
 using Nop.Core.Infrastructure.DependencyManagement;
+using Nop.Plugin.Company.Support.Areas.Admin.Factories;
 using Nop.Plugin.Company.Support.Services;
 
 namespace Nop.Plugin.Company.Support.Infrastructure
@@ -11,6 +12,7 @@ namespace Nop.Plugin.Company.Support.Infrastructure
         public virtual void Register(IServiceCollection services, ITypeFinder typeFinder, AppSettings appSettings)
         {
             services.AddScoped<ISupportCaseService, SupportCaseService>();
+            services.AddScoped<ISupportCaseModelFactory, SupportCaseModelFactory>();
         }
 
         public int Order => 3;
